58415242334c58b0a07e5a8dd625b9603ef11182
Quoc Khanh <quockhanh.abc@gmail.com>
2019-10-05 21:10:12 +0700

    update login controller

--- src/main/java/com/rentingbook/api/controller/JwtAuthenticationController.java
+++ src/main/java/com/rentingbook/api/controller/JwtAuthenticationController.java
@@ -33,7 +33,7 @@ public class JwtAuthenticationController {
         final UserDetails userDetails = userDetailsService
                 .loadUserByUsername(authenticationRequest.getUsername());
         final String token = jwtTokenUtil.generateToken(userDetails);
-        return ResponseEntity.ok(new JwtResponse(token));
+        return ResponseEntity.ok(token);
     }
 
     private void authenticate(String username, String password) throws Exception {
